c0a204c2600adc7d0925afa6298ade85e813d053
victor poirier <poiriervictor99@gmail.com>
2020-04-06 02:03:26 +0000

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  
+  resources :about, only: [:index]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+
 
   namespace :admin do
     root to: 'dashboard#show'
